efa98dcd4db64e6941527562156ba03b8a693413
Scott Trimber <scott.trimber@comcast.net>
2015-03-15 13:14:56 -0700

    [enhancement]   added code to implement functions
    Added code to implement makeCacheMatrix and cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## R Programming course
+## Scott Trimber
+## 3/15/2015
+## this function creates a cachable matrix that can be
+## reused if the contents are not changed.
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function will solve the inversion of matrix 'x'
+## If the matrix elements have not changed, it will return
+## the cached matrix to save computational time.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
